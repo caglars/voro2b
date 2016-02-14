e7762f87e8fe97ecc77eb7a3930e33078f293814
caglars <csinayuc@gmail.com>
2016-02-14 23:53:15 +0200

    works now with CLion, but before make and install voro++

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+using namespace std;
+
+#include <iostream>
+#include "voro++.hh"
+using namespace voro;
+
+#include "CSCalculation.h"
+
+int main() {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+
+    Calculation myCalculator;
+    myCalculator.calculate();
+    // insert code here...
+    std::cout << "Hello, World Again!\n";
+    return 0;
+}
